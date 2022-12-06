a558d42619de6f42f8a14b37f8ccf9ef861e3c88
GeonCws <hasaninci@hotmail.com.tr>
2022-12-06 13:10:20 +0300

    Test

--- Core/GeonAPI.Domain/Entities/Product.cs
+++ Core/GeonAPI.Domain/Entities/Product.cs
@@ -7,6 +7,7 @@ namespace GeonAPI.Domain.Entities
     {
         public string? Name { get; set; }
         public string? Description { get; set; }
+        public decimal Stocks { get; set; }
         public bool Visible { get; set; }
         public bool Deleted { get; set; }
         public Category Category { get; set; }

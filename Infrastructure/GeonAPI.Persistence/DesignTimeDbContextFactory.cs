86477b23938b32e7de1098e487c595f2c9400e0b
geoncws <114188847+geoncws@users.noreply.github.com>
2023-02-04 19:40:06 +0300

    Update DesignTimeDbContextFactory.cs

--- Infrastructure/GeonAPI.Persistence/DesignTimeDbContextFactory.cs
+++ Infrastructure/GeonAPI.Persistence/DesignTimeDbContextFactory.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Design;
 namespace GeonAPI.Persistence
 {
     public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<GeonAPIDbContext>
-    {
+    { 
         public GeonAPIDbContext CreateDbContext(string[] args)
         {
             DbContextOptionsBuilder<GeonAPIDbContext> dbContextOptionsBuilder = new();
